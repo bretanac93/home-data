3facbd055094c11d6b624d0a48e862a11dc81b0f
Cesar Bretana Gonzalez <bretanac@gmail.com>
2018-07-28 15:40:59 +0200

    Deselecting on sort

--- src/Components/SortAll/SortAll.js
+++ src/Components/SortAll/SortAll.js
@@ -3,26 +3,61 @@ import React, { Component } from 'react';
 class SortAll extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            price: '',
+            living_area_total: ''
+        };
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(evt) {
         const { name, value } = evt.target;
+
+        switch (name) {
+            case 'name':
+                this.setState({
+                    name: value,
+                    price: '',
+                    living_area_total: ''
+                });
+                break;
+            case 'price':
+                this.setState({
+                    name: '',
+                    price: value,
+                    living_area_total: ''
+                });
+                break;
+            case 'living_area_total':
+                this.setState({
+                    name: '',
+                    price: '',
+                    living_area_total: value
+                });
+                break;
+            default:
+                this.setState({
+                    name: '',
+                    price: '',
+                    living_area_total: ''
+                });
+        }
         this.props.orderByAll(name, value);
     }
     render() {
         return (
             <div className="SortAll">
-                <select name="name" id="name_sort" onChange={this.handleChange}>
+                <select name="name" value={this.state.name} id="name_sort" onChange={this.handleChange}>
                     <option value="">Name</option>
                     <option value="asc">Asc</option>
                     <option value="desc">Desc</option>
                 </select>
-                <select name="price" id="price_sort" onChange={this.handleChange}>
+                <select name="price" value={this.state.price} id="price_sort" onChange={this.handleChange}>
                     <option value="">Price</option>
                     <option value="asc">Asc</option>
                     <option value="desc">Desc</option>
                 </select>
-                <select name="living_area_total" id="size_sort" onChange={this.handleChange}>
+                <select name="living_area_total" value={this.state.living_area_total} id="size_sort" onChange={this.handleChange}>
                     <option value="">Size</option>
                     <option value="asc">Asc</option>
                     <option value="desc">Desc</option>
